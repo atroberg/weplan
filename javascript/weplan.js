810ac596cca961e5099f5d6f8c416ede957139eb
atroberg <alexistroberg@gmail.com>
2014-01-08 13:57:55 +0100

    Fixed the movement between search, list view and map view.
    * User can no longer manually scroll to a specific view (always must presh a button)
    * Animation between search and result views: zoom out (50%) and scroll to correct view and then zoom back in.
    * Animation between list and map view: slide horizontally
    * Movement between list and map view is now only possible by swipeleft/right/tap on the handles

--- javascript/weplan.js
+++ javascript/weplan.js
@@ -10,7 +10,7 @@ $('#search_results_list div.result').each(function() {
 });
 
 // Initialize the map
-var map = L.map('map').setView([64.886265, 29.047852], 2);
+var map = L.map('map').setView([64.886265, 29.047852], 4);
 L.tileLayer('http://mtile03.mqcdn.com/tiles/1.0.0/vy/map/{z}/{x}/{y}.png', {
 	attribution: '',
 	maxZoom: 18
@@ -22,11 +22,37 @@ function windowScrollTop(position) {
 	});
 }
 
+// Switching between list and map view
+$('#map_view_handle').hammer({
+	'swipe_velocity': 0.1
+}).on('swipeleft tap', function(e) {
+	$('#list_and_map_view').css('transform', 'translateX(-50%)');
+});
+$('#list_view_handle').hammer({
+	'swipe_velocity': 0.1
+}).on('swiperight tap', function(e) {
+	$('#list_and_map_view').css('transform', 'translateX(0px)');
+});
+
 // Search animation
+var resultsPositionTop;
 $('#search_button').hammer().on('tap', function(e) {
-	windowScrollTop($('#list_and_map_view_outer_container').offset().top);
+	$('#main_container').css('transform-origin', '50% 0%');
+	resultsPositionTop = -$('#list_and_map_view_outer_container').offset().top;
+
+	// The animation is much more smooth when using "transform",
+	// compared to modifying for example the "top"-property
+	$('#main_container').css('transform', 'translateY(0px) scale(0.5)');
+
+	setTimeout(function() {
+		$('#main_container').css('transform', 'translateY('+ resultsPositionTop + 'px) scale(1)');
+	}, 500);
 });
 
 $('button.back').hammer().on('tap', function(e) {
-	windowScrollTop(0);
+	$('#main_container').css('transform-origin', '50% 200%');
+	$('#main_container').css('transform', 'translateY('+ resultsPositionTop + 'px) scale(.5)');
+	setTimeout(function() {
+		$('#main_container').css('transform', 'translateY(0px) scale(1)');
+	}, 500);
 });
