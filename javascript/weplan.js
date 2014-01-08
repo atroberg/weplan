54337ca75051f82dcb89180fb7c0cc9338921546
atroberg <alexistroberg@gmail.com>
2014-01-08 22:36:29 +0100

    Basic structure for details view
    * Basic structure for details view, including the navigation between the tabs
    * Implemented the photos tab. It will automatically fetch photos using the Google image search API.

--- javascript/weplan.js
+++ javascript/weplan.js
@@ -50,9 +50,101 @@ $('#search_button').hammer().on('tap', function(e) {
 });
 
 $('button.back').hammer().on('tap', function(e) {
-	$('#main_container').css('transform-origin', '50% 200%');
-	$('#main_container').css('transform', 'translateY('+ resultsPositionTop + 'px) scale(.5)');
-	setTimeout(function() {
-		$('#main_container').css('transform', 'translateY(0px) scale(1)');
-	}, 400);
+	// Check if details view is visible => close it
+	if ( $('#details').hasClass('active') ) {
+		$('#details').removeClass('active');
+	}
+
+	else {
+		$('#main_container').css('transform-origin', '50% 200%');
+		$('#main_container').css('transform', 'translateY('+ resultsPositionTop + 'px) scale(.5)');
+		setTimeout(function() {
+			$('#main_container').css('transform', 'translateY(0px) scale(1)');
+		}, 400);
+	}
+});
+
+// Show details view when clicking result
+$('.result').hammer().on('tap', function(e) {
+	$('#details').addClass('active');
+});
+
+
+// Tabs in details view
+
+var currentActiveTab = 0;
+
+// Cache some selectors
+var detailsContainer = $('#details_container');
+var tabMenu = $('#details ul.tabs');
+
+tabMenu.find('li').hammer().on('tap', function() {
+	showTab($(this).index());
+});
+
+function showTab(index) {
+
+	// Check tab exists
+	if ( index < 0 || index > detailsContainer.find('.tab').length - 1 ) {
+		return;
+	}
+
+	currentActiveTab = index;
+
+	tabMenu.find('.active').removeClass('active');
+	tabMenu.find('li:eq(' + currentActiveTab + ')').addClass('active');
+
+	var value = -(index * 25) + '%';
+	detailsContainer.css('transform', 'translateX('+value+'%)');
+
+	// Check if we need to initialize tab with some content
+	
+	var tab = detailsContainer.find('.tab:eq(' + currentActiveTab + ')');
+
+	if ( tab.attr('data-initialized') != "true" ) {
+
+		tab.attr('data-initialized', 'true');
+
+		switch ( currentActiveTab ) {
+
+			default:
+				// By default we don't need to do anything
+				break;
+
+			// Photos
+			case 1:
+				// Fetch images from Google image search
+				$.ajax({
+					'url': 'https://ajax.googleapis.com/ajax/services/search/images?v=1.0',
+					'type': 'GET',
+					'data': {
+						'q': 'Nice, France',
+						'imgsz': 'medium|large',
+						'rsz': 8 // results per page
+					},
+					'dataType': 'jsonp',
+					'success': function(data) {
+						// Remove loading msg
+						tab.html('');
+
+						for( var i = 0; i < data.responseData.results.length; i ++ ) {
+							var result = data.responseData.results[i];
+		
+							var img = $('<div />').css('background-image', 'url("' + result.url + '")');
+							tab.append(img);
+						}
+					}
+				});
+				break;
+		}
+
+	}
+}
+
+$('#details_container').hammer({
+	'swipe_velocity': 0.1
+}).on('swipeleft', function() {
+	showTab(currentActiveTab + 1);
+}).on('swiperight', function() {
+	showTab(currentActiveTab - 1);
 });
