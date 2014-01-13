e6206bc6b640c3ec8307464dcb835beb14f32860
atroberg <alexistroberg@gmail.com>
2014-01-13 20:22:51 +0100

    Implemented the favorites functionality
    * Favorites are saved persistently with localStorage

--- javascript/weplan.js
+++ javascript/weplan.js
@@ -9,11 +9,17 @@ function onPopState(page) {
 	switch ( page ) {
 
 		case 'Frontpage':
-			$('#main_container').css('transform-origin', '50% 200%');
-			$('#main_container').css('transform', 'translateY('+ resultsPositionTop + 'px) scale(.5)');
-			setTimeout(function() {
-				$('#main_container').css('transform', 'translateY(0px) scale(1)');
-			}, 400);
+			if ( $('#favorites').hasClass('active') ) {
+				$('#favorites').removeClass('active');
+			}
+
+			else {
+				$('#main_container').css('transform-origin', '50% 200%');
+				$('#main_container').css('transform', 'translateY('+ resultsPositionTop + 'px) scale(.5)');
+				setTimeout(function() {
+					$('#main_container').css('transform', 'translateY(0px) scale(1)');
+				}, 400);
+			}
 
 			break;
 
@@ -58,6 +64,35 @@ function onPopState(page) {
 
 		case 'Sort':
 			$('#sort').addClass('active');	
+			break;
+
+		case 'Favorites':
+
+			if ( $('#details').hasClass('active') ) {
+				$('#details').removeClass('active');
+			}
+
+			$('#favorites').addClass('active');
+
+			// Get favorites from localStorage
+			var favorites = JSON.parse(localStorage.getItem("favorites"));
+
+			var favoritesFound = false;
+
+			for ( var listIndex in favorites ) {
+				if ( favorites[listIndex] ) {
+					showDestination(destinations[listIndex], listIndex, true, true);
+					favoritesFound = true;
+				}
+			}
+
+			if ( favoritesFound ) {
+				$('#favorites p.no_favorites').addClass('hidden');
+			}
+			else {
+				$('#favorites p.no_favorites').removeClass('hidden');
+			}
+
 			break;
 
 	}
@@ -120,7 +155,7 @@ var destinationInDetailsView;
 
 // Add destination to search results
 var destinationsToAddToDom = [];
-function showDestination(destination, listIndex) {
+function showDestination(destination, listIndex, isFavorited, addToFavoritesView) {
 	var result = $('#list_template').clone().removeAttr('id').attr('data-list-index', listIndex);
 
 	// Show details view when clicking result
@@ -142,6 +177,11 @@ function showDestination(destination, listIndex) {
 	destination.calculatedTemperature = temperature;
 	if ( temperature > 0 ) temperature = '+' + temperature;
 	result.find('.temperature').html('<i class="fa ' + icons[Math.floor(Math.random()*icons.length)] + '"></i> ' + temperature + '°C');
+
+	// Favorite icon
+	if ( isFavorited ) {
+		result.find('.favorite').addClass('active');
+	}
 
 	// Show random rating (0.5 - 3)
 	var rating = (0.0 + (1 + Math.floor(Math.random() * 5 + 1))) / 2.0;
@@ -174,26 +214,74 @@ function showDestination(destination, listIndex) {
 		activitiesEl.append('<i class="' + activities[destination.activities[i]] + '"></i> ');
 	}
 
-	destinationsToAddToDom.push(result);
+	// Favoriting
+	result.find('.favorite').hammer().on('tap', function(e) {
+		e.stopPropagation();
 
+		var el = $(this);
 
-	// UPDATING THE MAP VIEW
+		var listIndex = parseInt(el.parent().attr('data-list-index'));
 
-	var coordinates = [destination.latitude, destination.longitude];
+		// Get favorites from localStorage
+		var favorites = JSON.parse(localStorage.getItem("favorites"));
 
-	// Map bounds
-	if ( bounds == null ) {
-		bounds = L.latLngBounds([]);
+		// If not initialized before, do it now
+		if ( favorites == null ) favorites = {};
+		
+		// Not yet in favorites
+		if ( !el.hasClass('active') ) {
+			el.addClass('active');
+			showNotification("Destination added to favorites");
+			favorites[listIndex] = true;
+		}
+
+		// Already favorited
+		else {
+			el.removeClass('active');
+			showNotification("Destination removed from favorites");
+			favorites[listIndex] = false;
+
+			// If in favorites view, we also need to immediately remove the destination from the view
+			if ( addToFavoritesView ) {
+				el.parent().fadeOut(function() {
+					// Check if last favorited removed => show no favs text again
+					if ( $(this).parent().find('.result').length <= 1 ) {
+						$(this).parent().find('.no_favorites').removeClass('hidden');
+					}
+
+					$(this).remove();
+				});
+			}
+		}
+
+		// Save back to localStorage
+		localStorage.setItem("favorites", JSON.stringify(favorites));
+	});
+
+	if ( addToFavoritesView ) {
+		$('#favorites .page').append(result);
 	}
-	bounds.extend(coordinates);
+	else {
+		destinationsToAddToDom.push(result);
 
-	// Add marker to map
-	var markerObject = {
-		marker: L.marker(coordinates).addTo(map)
-	};
-	markerObject.marker.bindPopup("<h3>" + destination.city + ', ' + destination.country + "</h3>"+
-				"<button onclick='showDetails("+listIndex+")' ontouchend='showDetails("+listIndex+")' class='btn btn-primary'>Read More</button>");
-	mapMarkers[listIndex] = markerObject;
+		// UPDATING THE MAP VIEW
+
+		var coordinates = [destination.latitude, destination.longitude];
+
+		// Map bounds
+		if ( bounds == null ) {
+			bounds = L.latLngBounds([]);
+		}
+		bounds.extend(coordinates);
+
+		// Add marker to map
+		var markerObject = {
+			marker: L.marker(coordinates).addTo(map)
+		};
+		markerObject.marker.bindPopup("<h3>" + destination.city + ', ' + destination.country + "</h3>"+
+					"<button onclick='showDetails("+listIndex+")' ontouchend='showDetails("+listIndex+")' class='btn btn-primary'>Read More</button>");
+		mapMarkers[listIndex] = markerObject;
+	}
 
 	return destination;
 }
@@ -312,6 +400,9 @@ $('#search_button').hammer({
 
 		var resultCount = 0;
 
+		// Get favorites from localStorage
+		var favorites = JSON.parse(localStorage.getItem("favorites"));
+
 		for ( var i = 0; i < destinations.length; i ++ ) {
 			var destination = destinations[i];
 
@@ -323,7 +414,7 @@ $('#search_button').hammer({
 				|| ( maxBudget >= 501 && maxBudget <= 1000 && $.inArray(destination.price, [0, 1]) != -1 )
 				|| ( maxBudget >= 1001 ) ) {
 				
-				destination = showDestination(destination, i);
+				destination = showDestination(destination, i, favorites[i]);
 
 				resultCount ++;
 
@@ -622,3 +713,25 @@ function sortResults(results, sortCriteria, descSort) {
 		return result;
 	});
 }
+
+
+// Function for showing notifications
+function showNotification(msg, msgVisibleDuration) {
+
+	// Default duration
+	if ( typeof(msgVisibleDuration) == "undefined" ) msgVisibleDuration = 2500;
+
+	$('#notification').html(msg).addClass('active');
+
+	setTimeout(function() {
+		$('#notification').removeClass('active');
+	}, msgVisibleDuration);
+	
+}
+
+
+// Open favorites by clicking the button
+$('#show-favorites').hammer().on('tap', function(e) {
+	addHistory("Favorites");
+	onPopState("Favorites");
+});
